4182c498eea57216cdd780b8a46e26cd49c8332d
juan-pablo-perez-up-co <130450324+juan-pablo-perez-up-co@users.noreply.github.com>
2024-02-04 18:03:56 +0000

    Started Transaction Component

---
+++ composables/useCurrency.ts
@@ -0,0 +1,15 @@
+import { computed, isRef } from 'vue'
+import type { Ref } from 'vue'
+
+export const useCurrency = (amount: Ref<number> | number) => {
+  const currency = computed(() => {
+    return new Intl.NumberFormat('en-IN', {
+      style: 'currency',
+      currency: 'EUR',
+    }).format(isRef(amount) ? amount.value : amount)
+  })
+
+  return {
+    currency,
+  }
+}
